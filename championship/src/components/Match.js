5c4f19d8ce60e08fb93befb22ff516f45e4b2849
Ignacio1224 <ignaciocabrera1224@gmail.com>
2019-08-13 11:49:11 -0300

    Mejora de partidos, queda armarlos ponerlos en el redux y/o en la api y hacer las estadisticas

--- championship/src/components/Match.js
+++ championship/src/components/Match.js
@@ -1,234 +1,266 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import MatchTeam from "./MatchTeam";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import MatchTeam from './MatchTeam';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 class Match extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      message: null,
+		this.state = {
+			message: null,
 
-      team1: "",
-      team2: "",
-      matchEventsT1: [],
-      matchEventsT2: [],
-      unavailableTeams: []
-    };
-  }
+			team1: '',
+			team2: '',
+			matchEventsT1: [],
+			matchEventsT2: [],
+			unavailableTeams: []
+		};
+	}
 
-  changeTeam1 = ({ target: { value } }) => {
-    this.setState({ team1: value, team2: '', unavailableTeams: [...this.state.unavailableTeams, value] });
-    if (this.state.unavailableTeams.length > 0) {
-      this.setState({ unavailableTeams: [value] });
-    }
-  };
+	changeTeam1 = ({ target: { value } }) => {
+		this.setState({
+			team1: value,
+			team2: '',
+			unavailableTeams: [...this.state.unavailableTeams, value]
+		});
+		if (this.state.unavailableTeams.length > 0) {
+			this.setState({ unavailableTeams: [value] });
+		}
+	};
 
-  changeTeam2 = ({ target: { value } }) => this.setState({ team2: value });
+	changeTeam2 = ({ target: { value } }) => this.setState({ team2: value });
 
-  availableTeams = () => {
-    const {
-      team: { teams }
-    } = this.props;
+	availableTeams = () => {
+		const {
+			team: { teams }
+		} = this.props;
 
-    let avialiable = [];
+		let avialiable = [];
 
-    teams.forEach((t, i) => {
-      if (!this.state.unavailableTeams.includes(t._id))
-        avialiable = [...avialiable, t];
-    });
+		teams.forEach((t, i) => {
+			if (!this.state.unavailableTeams.includes(t._id))
+				avialiable = [...avialiable, t];
+		});
 
-    return avialiable;
-  };
+		return avialiable;
+	};
 
-  getTeam = teamId => {
-    const {
-      team: { teams }
-    } = this.props;
+	getTeam = teamId => {
+		const {
+			team: { teams }
+		} = this.props;
 
-    for (const t of teams) {
-      if (t._id === teamId) return t;
-    }
-  };
+		for (const t of teams) {
+			if (t._id === teamId) return t;
+		}
+	};
 
-  createMatch = event => {
-    event.preventDefault();
+	createMatch = event => {
+		event.preventDefault();
 
-    const { team1, team2 } = this.state;
+		const { team1, team2, matchEventsT1, matchEventsT2 } = this.state;
 
-    if (team1 !== "" && team2 !== "") {
-      this.setState({
-        message: {
-          className: "success",
-          title: "Sapee!",
-          message: "Pin y verde pari!"
-        }
-      });
-    } else {
-      this.setState({
-        message: {
-          className: "danger",
-          title: "Error!",
-          message: "No ha completado todos los campos!"
-        }
-      });
-    }
-    // console.log(team1, team2);
-  };
+		if (team1 !== '' && team2 !== '') {
+			this.setState({
+				message: {
+					className: 'success',
+					title: 'Sapee!',
+					message: 'Pin y verde pari!'
+				}
+			});
+		} else {
+			this.setState({
+				message: {
+					className: 'danger',
+					title: 'Error!',
+					message: 'No ha completado todos los campos!'
+				}
+			});
+		}
+		console.log(matchEventsT1, matchEventsT2);
+	};
 
-  addEventT1 = ev => {
-    this.setState({ matchEventsT1: [...this.state.matchEventsT1, { ...ev }] });
-  };
+	addEventT1 = ev => {
+		this.setState({
+			matchEventsT1: [...this.state.matchEventsT1, { ...ev }]
+		});
+	};
 
-  addEventT2 = ev => {
-    this.setState({ matchEventsT2: [...this.state.matchEventsT2, { ...ev }] });
-  };
+	addEventT2 = ev => {
+		this.setState({
+			matchEventsT2: [...this.state.matchEventsT2, { ...ev }]
+		});
+	};
 
-  render() {
-    const {
-      //   championship: { isConfirmed }
-      team: { teams }
-    } = this.props;
+	render() {
+		const {
+			//   championship: { isConfirmed }
+			team: { teams }
+		} = this.props;
 
-    const { team1, team2, message, matchEventsT1, matchEventsT2 } = this.state;
-    const avTeams = this.availableTeams();
-    console.log(matchEventsT1);
+		const {
+			team1,
+			team2,
+			message
+			// matchEventsT1,
+			// matchEventsT2
+		} = this.state;
+		const avTeams = this.availableTeams();
+		// console.log(matchEventsT1);
 
+		let isConfirmed = true; // TODO HARCODED!!!
 
-    let isConfirmed = true; // TODO HARCODED!!!
+		return (
+			<div className='row'>
+				<div className='col-12'>
+					<h2 className='text-center'>Partido</h2>
+				</div>
+				<div className='col-12 mt-5'>
+					{isConfirmed ? (
+						<form onSubmit={this.createMatch}>
+							<div className='row'>
+								<div className='form-group col-5'>
+									<label htmlFor='selectMatchTeam1'>
+										Equipo 1
+									</label>
+									<select
+										className='form-control'
+										id='selectMatchTeam1'
+										onChange={this.changeTeam1}
+										value={team1}
+									>
+										<option value=''>------</option>
+										{teams.map((tmp, index) => (
+											<option key={index} value={tmp._id}>
+												{tmp.name}
+											</option>
+										))}
+									</select>
+								</div>
+								<div className='form-group col-5 offset-1'>
+									<label htmlFor='selectMatchTeam2'>
+										Equipo 2
+									</label>
+									<select
+										className='form-control'
+										id='selectMatchTeam2'
+										onChange={this.changeTeam2}
+										value={team2}
+										disabled={
+											avTeams.length === teams.length
+										}
+									>
+										<option value=''>------</option>
+										{avTeams.map((tmp, index) => (
+											<option key={index} value={tmp._id}>
+												{tmp.name}
+											</option>
+										))}
+									</select>
+								</div>
 
-    return (
-      <div className="row">
-        <div className="col-12">
-          <h2 className="text-center">Partido</h2>
-        </div>
-        <div className="col-12 mt-5">
-          {isConfirmed ? (
-            <form onSubmit={this.createMatch}>
-              <div className="row">
-                <div className="form-group col-5">
-                  <label htmlFor="selectMatchTeam1">Equipo 1</label>
-                  <select
-                    className="form-control"
-                    id="selectMatchTeam1"
-                    onChange={this.changeTeam1}
-                    value={team1}
-                  >
-                    <option value="">------</option>
-                    {teams.map((tmp, index) => (
-                      <option key={index} value={tmp._id}>
-                        {tmp.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="form-group col-5 offset-1">
-                  <label htmlFor="selectMatchTeam2">Equipo 2</label>
-                  <select
-                    className="form-control"
-                    id="selectMatchTeam2"
-                    onChange={this.changeTeam2}
-                    value={team2}
-                    disabled={avTeams.length === teams.length}
-                  >
-                    <option value="">------</option>
-                    {avTeams.map((tmp, index) => (
-                      <option key={index} value={tmp._id}>
-                        {tmp.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+								{team1 !== '' && team2 !== '' && (
+									<>
+										<MatchTeam
+											team={this.getTeam(team1)}
+											addEvent={this.addEventT1}
+										/>
+										<MatchTeam
+											team={this.getTeam(team2)}
+											addEvent={this.addEventT2}
+										/>
+									</>
+								)}
+							</div>
 
-                {team1 !== "" && team2 !== "" && (
-                  <>
-                    <MatchTeam
-                      team={this.getTeam(team1)}
-                      addEvent={this.addEventT1}
-                    />
-                    <MatchTeam
-                      team={this.getTeam(team2)}
-                      addEvent={this.addEventT2}
-                    />
-                  </>
-                )}
-              </div>
-              {
-                (matchEventsT1.length > 0 || matchEventsT2.length > 0) && (
-                  <div className='col-12 mt-2'>
-                    <h5 className='text-center'>Eventos registrados</h5>
-                    
-                    <div className="col-5">
-                      {matchEventsT1.map((e, i) => (
-                        <div key={i} className="alert alert-primary" role='alert'>
-                          {e.minute} - {e.mEvent}
-                        </div>
-                      ))}
-                    </div>
-                    
-                    <div className="col-5 offset-1">
-                      {matchEventsT2.map((e, i) => (
-                        <div key={i} className="alert alert-primary" role='alert'>
-                          {e.minute} - {e.mEvent}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                )
-              }
-              
-              <div className="form-group row mt-2">
-                <div className="col-sm-10">
-                  <button type="submit" className="btn btn-success">
-                    <FontAwesomeIcon icon={faPlus} />
-                    Crear
-                  </button>
-                </div>
-              </div>
+							{/* {(matchEventsT1.length > 0 ||
+								matchEventsT2.length > 0) && (
+								<div className='row mt-2'>
+									<div className='col-12 mt-2'>
+										<h5 className='text-center'>
+											Eventos registrados
+										</h5>
+									</div>
+									{matchEventsT1.map((e, i) => (
+										<div
+											key={i}
+											className='col-5 alert alert-primary'
+											role='alert'
+										>
+											{e.minute} - {e.mEvent}
+										</div>
+									))}
 
-              {message && (
-                <div className="custom-alert">
-                  <div
-                    className={`alert alert-${
-                      message.className
-                      } alert-dismissible fade show`}
-                    role="alert"
-                  >
-                    <strong>{message.title}</strong> {message.message}
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                      onClick={() =>
-                        this.setState({
-                          message: null
-                        })
-                      }
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                </div>
-              )}
-            </form>
-          ) : (
-              <h1>No se ha iniciado el campeonato!.</h1>
-            )}
-        </div>
-      </div>
-    );
-  }
+									{matchEventsT2.map((e, i) => (
+										<span
+											key={i}
+											className='col-5 offset-1 alert alert-primary'
+											role='alert'
+										>
+											{e.minute} - {e.mEvent}
+										</span>
+									))}
+								</div>
+							)} */}
+
+							<div className='form-group row mt-2'>
+								<div className='col-sm-10'>
+									<button
+										type='submit'
+										className='btn btn-success'
+									>
+										<FontAwesomeIcon icon={faPlus} />
+										Crear
+									</button>
+								</div>
+							</div>
+
+							{message && (
+								<div className='custom-alert'>
+									<div
+										className={`alert alert-${
+											message.className
+										} alert-dismissible fade show`}
+										role='alert'
+									>
+										<strong>{message.title}</strong>{' '}
+										{message.message}
+										<button
+											type='button'
+											className='close'
+											data-dismiss='alert'
+											aria-label='Close'
+											onClick={() =>
+												this.setState({
+													message: null
+												})
+											}
+										>
+											<span aria-hidden='true'>
+												&times;
+											</span>
+										</button>
+									</div>
+								</div>
+							)}
+						</form>
+					) : (
+						<h1>No se ha iniciado el campeonato!.</h1>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    user: state.userReducer.user,
-    championship: state.championshipReducer.championship,
-    team: state.teamReducer
-  };
+	return {
+		user: state.userReducer.user,
+		championship: state.championshipReducer.championship,
+		team: state.teamReducer
+	};
 }
 
 export default connect(mapStateToProps)(Match);
--- championship/src/components/MatchTeam.js
+++ championship/src/components/MatchTeam.js
@@ -1,174 +1,172 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 class MatchTeam extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      message: null,
+		this.state = {
+			message: null,
 
-      // {player: id, time: time, event}
-      // event could be ('goal, yellow card, red card')
-      teamPlayers: [],
-      player: "",
-      minute: "",
-      mEvent: ""
-    };
+			// {player: id, time: time, event}
+			// event could be ('goal, yellow card, red card')
+			teamPlayers: [],
+			player: '',
+			minute: '',
+			mEvent: ''
+		};
 
-    this.matchEvents = ["Gol", "Tarjeta amarilla", "Expulsión"];
-    this.minutes = Array(90);
-    this.loadMinutes();
-  }
+		this.matchEvents = ['Gol', 'Tarjeta amarilla', 'Expulsión'];
+	}
 
-  changePlayer = ({ target: { value } }) => this.setState({ player: value });
-  changeMinute = ({ target: { value } }) => this.setState({ minute: value });
-  changeEvent = ({ target: { value } }) => this.setState({ mEvent: value });
+	changePlayer = ({ target: { value } }) => this.setState({ player: value });
+	changeMinute = ({ target: { value } }) => this.setState({ minute: value });
+	changeEvent = ({ target: { value } }) => this.setState({ mEvent: value });
 
-  loadMinutes = () => {
-    for (let i = 1; i <= 90; i++) {
-      this.minutes.push(i);
-    }
-  };
+	addLine = event => {
+		event.preventDefault();
 
-  addLine = event => {
-    event.preventDefault();
+		const { player, minute, mEvent } = this.state;
+		const { addEvent } = this.props;
 
-    const { player, minute, mEvent } = this.state;
-    const { addEvent } = this.props;
+		if (player === '' || minute === '' || mEvent === '') {
+			return this.setState({
+				message: {
+					title: 'Oooops!',
+					message: 'Faltan datos!',
+					className: 'danger'
+				}
+			});
+		}
 
-    if (player === "" || minute === "" || mEvent === "") {
-      return this.setState({
-        message: {
-          title: "Oooops!",
-          message: "Faltan datos!",
-          className: "danger"
-        }
-      });
-    }
+		addEvent({
+			player,
+			minute,
+			mEvent
+		});
 
-    addEvent({
-      player,
-      minute,
-      mEvent
-    });
+		return this.setState({
+			message: {
+				title: 'Correcto!',
+				message: 'Se ingresaron los datos!',
+				className: 'success'
+			},
+			player: '',
+			minute: '',
+			mEvent: ''
+		});
+	};
 
-    return this.setState({
-      message: {
-        title: "Correcto!",
-        message: "Se ingresaron los datos!",
-        className: "success"
-      },
-      player: "",
-      minute: "",
-      mEvent: ""
-    });
-  };
+	render() {
+		const { player, minute, mEvent, message } = this.state;
 
-  render() {
-    const { player, minute, mEvent, message } = this.state;
+		const {
+			team: { name, players }
+		} = this.props;
 
-    const {
-      team: { name, players }
-	} = this.props;
-	// console.log(players);
-	
-
-    return (
-      <div className="col-6">
-        <div className="card">
-          <div className="card-body">
-            <div className="card-title">
-              <h4>{`Equipo ${name}`}</h4>
-            </div>
-            <div className="card-text">
-              {message && (
-                <div className="custom-alert">
-                  <div
-                    className={`alert alert-${
-                      message.className
-                    } alert-dismissible fade show`}
-                    role="alert"
-                  >
-                    <strong>{message.title}</strong> {message.message}
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                      onClick={() => this.setState({ message: null })}
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                </div>
-              )}
-              <div className="form-group">
-                <label htmlFor={`selectMatchPlayer${player.number}`}>Jugador</label>
-                <select
-                  className="form-control"
-                  id={`selectMatchPlayer${player.number}`}
-                  onChange={this.changePlayer}
-                  value={player}
-                >
-                  <option value="">------</option>
-                  {players.map((p, index) => (
-                    <option
-                      key={index}
-                      value={p.number}
-                    >
-                      Nombre: {p.name} Apellido: {p.lastName} Nº camiseta: {p.number}
-                    </option>
-                  ))}
-                </select>
-                <label htmlFor={`selectMinute${player.number}`}>Minuto</label>
-                <select
-                  className="form-control"
-                  id={`selectMinute${player.number}`}
-                  onChange={this.changeMinute}
-                  value={minute}
-                >
-                  <option value="">------</option>
-                  {this.minutes.map((m, index) => (
-                    <option key={index} value={`${m}`}>
-                      {m}
-                    </option>
-                  ))}
-                </select>
-                <label htmlFor={`selectEvent${player.minute}`}>Evento</label>
-                <select
-                  className="form-control"
-                  id={`selectEvent${player.minute}`}
-                  onChange={this.changeEvent}
-                  value={mEvent}
-                >
-                  <option value="">------</option>
-                  {this.matchEvents.map((e, index) => (
-                    <option key={index} value={`${e}`}>
-                      {e}
-                    </option>
-                  ))}
-                </select>
-                <div className="form-group row mt-2">
-                  <div className="col-sm-10">
-                    <button
-                      type="submit"
-                      className="btn btn-success"
-                      onClick={this.addLine}
-                    >
-                      <FontAwesomeIcon icon={faPlus} />
-                      Añadir
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className='col-6'>
+				<div className='card'>
+					<div className='card-body'>
+						<div className='card-title'>
+							<h4>{`Equipo ${name}`}</h4>
+						</div>
+						<div className='card-text'>
+							{message && (
+								<div className='custom-alert'>
+									<div
+										className={`alert alert-${
+											message.className
+										} alert-dismissible fade show`}
+										role='alert'
+									>
+										<strong>{message.title}</strong>{' '}
+										{message.message}
+										<button
+											type='button'
+											className='close'
+											data-dismiss='alert'
+											aria-label='Close'
+											onClick={() =>
+												this.setState({ message: null })
+											}
+										>
+											<span aria-hidden='true'>
+												&times;
+											</span>
+										</button>
+									</div>
+								</div>
+							)}
+							<div className='form-group'>
+								<label
+									htmlFor={`selectMatchPlayer${
+										player.number
+									}`}
+								>
+									Jugador
+								</label>
+								<select
+									className='form-control'
+									id={`selectMatchPlayer${player.number}`}
+									onChange={this.changePlayer}
+									value={player}
+								>
+									<option value=''>------</option>
+									{players.map((p, index) => (
+										<option key={index} value={p.number}>
+											Nombre: {p.name} Apellido:{' '}
+											{p.lastName} Nº camiseta: {p.number}
+										</option>
+									))}
+								</select>
+								<label htmlFor={`selectMinute${player.number}`}>
+									Minuto
+								</label>
+								<input
+									type='number'
+									min='1'
+									className='form-control'
+									id={`selectMinute${player.number}`}
+									onChange={this.changeMinute}
+									value={minute}
+								/>
+								<label htmlFor={`selectEvent${player.minute}`}>
+									Evento
+								</label>
+								<select
+									className='form-control'
+									id={`selectEvent${player.minute}`}
+									onChange={this.changeEvent}
+									value={mEvent}
+								>
+									<option value=''>------</option>
+									{this.matchEvents.map((e, index) => (
+										<option key={index} value={`${e}`}>
+											{e}
+										</option>
+									))}
+								</select>
+								<div className='form-group row mt-2'>
+									<div className='col-sm-10'>
+										<button
+											type='submit'
+											className='btn btn-primary'
+											onClick={this.addLine}
+										>
+											<FontAwesomeIcon icon={faPlus} />
+											Añadir
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default MatchTeam;
